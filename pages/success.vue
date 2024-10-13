0dff525610e52854850548156ef1c3b6783f97a0
Damian Fiałkiewicz <aekal4@gmail.com>
2024-10-13 20:44:25 +0200

    Add navigation guard for success page

--- pages/success.vue
+++ pages/success.vue
@@ -1,3 +1,13 @@
+<script setup>
+definePageMeta({
+  middleware: defineNuxtRouteMiddleware((_to, from) => {
+    if (from.name !== 'index') {
+      return navigateTo({ name: 'index' });
+    }
+  }),
+})
+</script>
+
 <template>
   <nord-stack class="centered-box">
     <nord-banner variant="success">
